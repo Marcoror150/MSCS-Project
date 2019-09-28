4ba463e866a88bd07f793eed58ae3cde4abd5eef
dannymulick1 <danny.mulick1@marist.edu>
2019-09-28 14:49:49 -0400

    Base app changed to follow template part ii

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-
+# Routing is how we will set up the pages, we basically fire off this app as 
+#  things come in
 @app.route('/')
 def hello():
     return "Hello World!"
