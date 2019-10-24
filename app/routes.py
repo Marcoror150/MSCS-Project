9eb76d421a644f25187f253d8e4a143435570f8d
Marc Christensen <marc.christensen1@marist.edu>
2019-10-24 17:57:25 -0400

    Added button.

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template, url_for
 
 @app.route('/')
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template("index.html")
 @app.route('/multiple_fatalities')
