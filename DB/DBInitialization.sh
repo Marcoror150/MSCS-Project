6d4a576911624d390db6235e90252e69c4883183
Marc Christensen <marc.christensen1@marist.edu>
2019-11-07 12:43:49 -0500

    Created basic script to install, initialize, and populate database for our application. Needs to be tested on a fresh host.

---
+++ DB/DBInitialization.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install Ruby and Homebrew.
+sudo apt update
+
+sudo apt install ruby-full
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install postgresql.
+brew install postgresql
+
+# Start postgresql services everytime on startup.
+pg_ctl -D /usr/local/var/postgres start && brew services start postgresql
+
+# Check version for debugging purposes.
+# postgres -V
+
+# Sudo may be required to enter postgresql environment.
+psql postgres
+
+# Create a user
+CREATE ROLE postgres WITH LOGIN PASSWORD 'password';
+
+# Grant user permissions
+ALTER ROLE postgres CREATEDB;
+
+# Leave super user environment and enter postgres user environment.
+\q
+psql postgres -U postgres
+
+# Create database for application and assign postgres as owner.
+CREATE DATABASE accidents_raw;
+GRANT ALL PRIVILEGES ON DATABASE accidents_raw TO postgres;
+
+# Connect to the DB.
+\connect accidents_raw 
+
+# Run DB creation script. May need to specify python2?
+\q
+python DBPopulate.py
