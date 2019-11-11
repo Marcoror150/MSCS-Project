ca37d93d1015f28292ad3ba320d47bd2a7c5104b
dannymulick1 <danny.mulick1@marist.edu>
2019-11-11 16:02:27 -0500

    Trying to add options to make it able to stand up on AWS, will update to WSGI before finals

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes, errors
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run(host="0.0.0.0", port=80)
