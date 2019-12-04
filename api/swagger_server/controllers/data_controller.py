db491d4cd8a0ec235ce9a2edd66fdf8520c5be8b
dannymulick1 <danny.mulick1@marist.edu>
2019-12-04 18:24:15 -0500

    data controller done

--- api/swagger_server/controllers/data_controller.py
+++ api/swagger_server/controllers/data_controller.py
@@ -67,6 +67,8 @@ def get_data(st_case=None, make=None, model=None, mod_year=None, fatals=None):
     toRemove.reverse()
     for j in toRemove:
         toCheck.pop(j)
+    if toCheck == []:
+        statement = "SELECT * FROM accident_vehicle_master"
 
     if st_case != None and make != None and model != None and \
         mod_year != None and fatals != None:
@@ -99,12 +101,12 @@ def get_data(st_case=None, make=None, model=None, mod_year=None, fatals=None):
 
         statement += ") AND MOD_YEAR IN ("
         if len(mod_year) > 1:
-            for stateName in state:
-                statement += stateName
-                if stateName != state[-1]:
+            for year in mod_year:
+                statement += str(year)
+                if year != mod_year[-1]:
                     statement += ", "
         else:
-            statement += state[0]
+            statement += str(mod_year[0])
 
         statement += ") AND FATALS IN ("
         if len(fatals) > 1:
@@ -116,83 +118,82 @@ def get_data(st_case=None, make=None, model=None, mod_year=None, fatals=None):
             statement += str(fatals[0])
         statement += ")"
 
-    elif st_case != None:
-        statement += " ST_CASE IN ("
-        if len(st_case) > 1:
-            for caseNum in st_case:
-                statement += caseNum
-                if caseNum != st_case[-1]:
-                    statement += ", "
-        else:
-            statement += st_case[0]
-        toCheck.remove(st_case)
-        if len(toCheck) > 0:
-            statement += ") AND"
-        else:
-            statement += ")"
-
-    elif make != None:
-        statement += " MAKE IN ("
-        if len(make) > 1:
-            for makeName in make:
-                statement += makeName
-                if makeName != make[-1]:
-                    statement += ", "
-        else:
-            statement += make[0]
-        toCheck.remove(make)
-        if len(toCheck) > 0:
-            statement += ") AND"
-        else:
-            statement += ")"
-
-    elif model != None:
-        statement += " MODEL IN ("
-        if len(model) > 1:
-            for modelName in model:
-                statement += modelName
-                if modelName != model[-1]:
-                    statement += ", "
-        else:
-            statement += model[0]
-        toCheck.remove(model)
-        if len(toCheck) > 0:
-            statement += ") AND"
-        else:
-            statement += ")"
-
-    elif mod_year != None:
-        statement += " MOD_YEAR IN ("
-        if len(mod_year) > 1:
-            for year in mod_year:
-                statement += year
-                if year != mod_year[-1]:
-                    statement += ", "
-        else:
-            statement += mod_year[0]
-        toCheck.remove(mod_year)
-        if len(toCheck) > 0:
-            statement += ") AND"
-        else:
-            statement += ")"
-
-    elif fatals != None:
-        statement += " FATALS IN ("
-        if len(fatals) > 1:
-            for fatal in fatals:
-                statement += str(fatal)
-                if fatal != fatals[-1]:
-                    statement += ", "
-        else:
-            statement += str(fatals[0])
-        toCheck.remove(fatals)
-        if len(toCheck) > 0:
-            statement += ") AND"
-        else:
-            statement += ")"
-
     else:
-        statement = "SELECT * FROM accident_vehicle_master"
+        if st_case != None:
+            statement += " ST_CASE IN ("
+            if len(st_case) > 1:
+                for caseNum in st_case:
+                    statement += caseNum
+                    if caseNum != st_case[-1]:
+                        statement += ", "
+            else:
+                statement += st_case[0]
+            toCheck.remove(st_case)
+            if len(toCheck) > 0:
+                statement += ") AND"
+            else:
+                statement += ")"
+
+        if make != None:
+            statement += " MAKE IN ("
+            if len(make) > 1:
+                for makeName in make:
+                    statement += makeName
+                    if makeName != make[-1]:
+                        statement += ", "
+            else:
+                statement += make[0]
+            toCheck.remove(make)
+            if len(toCheck) > 0:
+                statement += ") AND"
+            else:
+                statement += ")"
+
+        if model != None:
+            statement += " MODEL IN ("
+            if len(model) > 1:
+                for modelName in model:
+                    statement += modelName
+                    if modelName != model[-1]:
+                        statement += ", "
+            else:
+                statement += model[0]
+            toCheck.remove(model)
+            if len(toCheck) > 0:
+                statement += ") AND"
+            else:
+                statement += ")"
+
+        if mod_year != None:
+            statement += " MOD_YEAR IN ("
+            if len(mod_year) > 1:
+                for year in mod_year:
+                    statement += str(year)
+                    if year != mod_year[-1]:
+                        statement += ", "
+            else:
+                statement += str(mod_year[0])
+            toCheck.remove(mod_year)
+            if len(toCheck) > 0:
+                statement += ") AND"
+            else:
+                statement += ")"
+
+        if fatals != None:
+            statement += " FATALS IN ("
+            if len(fatals) > 1:
+                for fatal in fatals:
+                    statement += str(fatal)
+                    if fatal != fatals[-1]:
+                        statement += ", "
+            else:
+                statement += str(fatals[0])
+            toCheck.remove(fatals)
+            if len(toCheck) > 0:
+                statement += ") AND"
+            else:
+                statement += ")"
+
     statement += ";"
     cur.execute(statement)
 
